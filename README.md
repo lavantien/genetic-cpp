d5cb2170a3f1e9a9355f0fec9699f0722611df9b
lavantien <lavantien96@gmail.com>
2023-06-06 01:46:24 +0700

    upgrade to gcc-13 and rerun on faster machine

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# genetic-cpp
+# Genetic CPP
+
+```bash
+./compile.sh
+```
+
+```bash
+./main
+```
+
+```bash
+tail output.txt -n 8
+```
+
+```bash
+Generation 9999 with 10000 individuals. Average fitness: 59.0082 -> Current best fitness:
+[187, 135, 95, 61, 163, 153, 48, 8, 185, 38, 9, 111, 165, 83, 115, 178, 186, 167, 68, 189, 41, 150, 97, 47, 144, 128, 152, 29, 155, 122, 143, 55, 181, 53, 51, 142, 127, 21, 161, 39, 3, 126, 164, 190, 107, 108, 82, 74, 19, 69, 33, 162, 65, 94, 101, 112, 182, 86, 194, 42, 59, 5, 49, 156, 198, 26, 132, 72, 146, 0, 13, 104, 106, 71, 16, 157, 6, 105, 160, 24, 154, 32, 117, 183, 140, 172, 23, 96, 125, 130, 34, 179, 180, 50, 88, 120, 58, 15, 129, 4, 191, 44, 148, 75, 134, 138, 18, 92, 63, 139, 22, 109, 67, 70, 81, 133, 10, 193, 27, 188, 177, 31, 137, 90, 35, 89, 174, 80, 151, 60, 145, 12, 119, 56, 99, 102, 199, 54, 45, 166, 131, 85, 100, 116, 37, 62, 197, 78, 195, 28, 52, 159, 168, 124, 93, 110, 77, 192, 113, 169, 40, 114, 64, 30, 158, 57, 98, 184, 43, 149, 123, 73, 25, 36, 136, 173, 7, 11, 76, 118, 87, 79, 147, 66, 46, 170, 1, 141, 196, 84, 176, 2, 175, 20, 91, 103, 14, 171, 17, 121] -> 59.009108
+
+Generation 10000 with 10000 individuals. Average fitness: 59.0082 -> Current best fitness:
+[187, 135, 95, 61, 163, 153, 48, 8, 185, 38, 9, 111, 165, 83, 115, 178, 186, 167, 68, 189, 41, 150, 97, 47, 144, 128, 152, 29, 155, 122, 143, 55, 181, 53, 51, 142, 127, 21, 161, 39, 3, 126, 164, 190, 107, 108, 82, 74, 19, 69, 33, 162, 65, 94, 101, 112, 182, 86, 194, 42, 59, 5, 49, 156, 198, 26, 132, 72, 146, 0, 13, 104, 106, 71, 16, 157, 6, 105, 160, 24, 154, 32, 117, 183, 140, 172, 23, 96, 125, 130, 34, 179, 180, 50, 88, 120, 58, 15, 129, 4, 191, 44, 148, 75, 134, 138, 18, 92, 63, 139, 22, 109, 67, 70, 81, 133, 10, 193, 27, 188, 177, 31, 137, 90, 35, 89, 174, 80, 151, 60, 145, 12, 119, 56, 99, 102, 199, 54, 45, 166, 131, 85, 100, 116, 37, 62, 197, 78, 195, 28, 52, 159, 168, 124, 93, 110, 77, 192, 113, 169, 40, 114, 64, 30, 158, 57, 98, 184, 43, 149, 123, 73, 25, 36, 136, 173, 7, 11, 76, 118, 87, 79, 147, 66, 46, 170, 1, 141, 196, 84, 176, 2, 175, 20, 91, 103, 14, 171, 17, 121] -> 59.009108
+Mutation occurs 8 times
+Done!
+Execution time: 172s
+```
+
+```bash
+            .-/+oossssoo+/-.               lavantien@savaka
+        `:+ssssssssssssssssss+:`           ----------------
+      -+ssssssssssssssssssyyssss+-         OS: Ubuntu 23.04 x86_64
+    .ossssssssssssssssssdMMMNysssso.       Host: MS-7D42 1.0
+   /ssssssssssshdmmNNmmyNMMMMhssssss/      Kernel: 6.2.0-20-generic
+  +ssssssssshmydMMMMMMMNddddyssssssss+     Uptime: 8 hours, 4 mins
+ /sssssssshNMMMyhhyyyyhmNMMMNhssssssss/    Packages: 2530 (dpkg), 162 (brew), 6 (flatpak), 26 (snap)
+.ssssssssdMMMNhsssssssssshNMMMdssssssss.   Shell: zsh 5.9
++sssshhhyNMMNyssssssssssssyNMMMysssssss+   Resolution: 3840x2160
+ossyNMMMNyMMhsssssssssssssshmmmhssssssso   DE: GNOME 44.0
+ossyNMMMNyMMhsssssssssssssshmmmhssssssso   WM: Mutter
++sssshhhyNMMNyssssssssssssyNMMMysssssss+   WM Theme: Adwaita
+.ssssssssdMMMNhsssssssssshNMMMdssssssss.   Theme: Yaru-dark [GTK2/3]
+ /sssssssshNMMMyhhyyyyhdNMMMNhssssssss/    Icons: Yaru [GTK2/3]
+  +sssssssssdmydMMMMMMMMddddyssssssss+     Terminal: WezTerm
+   /ssssssssssshdmNNNNmyNMMMMhssssss/      CPU: 12th Gen Intel i7-12700F (20) @ 4.800GHz
+    .ossssssssssssssssssdMMMNysssso.       GPU: NVIDIA GeForce RTX 3080 Lite Hash Rate
+      -+sssssssssssssssssyyyssss+-         Memory: 6507MiB / 31930MiB
+        `:+ssssssssssssssssss+:`
+            .-/+oossssoo+/-.
+
+```
---
+++ main.cpp
@@ -0,0 +1,257 @@
+// Tested with Visual Studio 2019 - version 16.4.0 and gcc-13
+//
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
+using namespace std;
+
+// constants
+
+static const double LOCATION_RANGE = 10000.0; // Map size in km
+static const int POPULATION_SIZE = 10000;
+static const int DNA_SIZE = 200; // Number of cities, a city is defined by its
+                                 // id and its location (x, y)
+static const int GENERATION_COUNT = 10000; // Number of evolution loops
+static const double MUTATION_CHANCE = 0.001;
+static const double SELECTION_SIZE =
+    0.666667; // Select ~2/3 top individuals each loop
+static const double CROSSOVER_POINT =
+    0.5; // Child inherit 50% of father genes and 50% of mother genes
+
+// class prototypes
+
+class Gene {
+public:
+  Gene(const int id, const double x, const double y);
+  const double calcDistance(const Gene &city) const;
+  const int getId() const { return id; }
+  const string toString() const;
+
+private:
+  int id;
+  double x;
+  double y;
+};
+
+class Chromosome {
+public:
+  Chromosome(const vector<Gene> &cities);
+  const string toString() const;
+  const vector<Gene> &getDNA() const;
+  const double getFitness() const;
+
+private:
+  const void evaluateFitness();
+
+private:
+  vector<Gene> DNA;
+  double fitness;
+};
+
+// global variables
+
+ofstream out("output.txt");
+
+random_device rd{};
+mt19937 gen{rd()};
+uniform_real_distribution<double> distributionLocation{-LOCATION_RANGE,
+                                                       LOCATION_RANGE};
+uniform_real_distribution<double> distributionPercentage{0.0, 1.0};
+
+vector<Gene> originalDNA;
+vector<Chromosome> population;
+
+int mutationCount;
+double avgFitness;
+
+// function prototypes
+
+void initPopulation(const int chromosomesCount, const int genesCount = 0);
+void printDNA(const Chromosome &individual);
+void populationSort();
+void selection();
+void printIndividualWithBestFitness(const int generation);
+void crossover();
+void mutation();
+void calcAvgFitness();
+
+// main function
+
+int main() {
+  cout << "Running..." << endl;
+
+  auto started = chrono::high_resolution_clock::now();
+
+  initPopulation(POPULATION_SIZE, DNA_SIZE);
+  out << "Input (original individual):" << endl;
+  printDNA(originalDNA);
+  for (int i = 0; i < GENERATION_COUNT; ++i) {
+    calcAvgFitness();
+    selection();
+    printIndividualWithBestFitness(i);
+    crossover();
+    mutation();
+    /*out << i << "th - size: " << population.size() << " individuals:" << endl;
+    for (auto individual : population) {
+            out << individual.toString() << endl;
+    }*/
+  }
+  populationSort();
+  printIndividualWithBestFitness(GENERATION_COUNT);
+  out << "Mutation occurs " << mutationCount << " times" << endl;
+
+  auto done = chrono::high_resolution_clock::now();
+
+  out << "Done!\nExecution time: "
+      << chrono::duration_cast<chrono::seconds>(done - started).count() << "s"
+      << endl;
+  cout << "Done!\nExecution time: "
+       << chrono::duration_cast<chrono::seconds>(done - started).count() << "s"
+       << endl;
+
+  out.close();
+  return 0;
+}
+
+// Gene implementation
+
+Gene::Gene(const int id, const double x, const double y) {
+  this->id = id;
+  this->x = x;
+  this->y = y;
+}
+
+const double Gene::calcDistance(const Gene &city) const {
+  return sqrt((this->x - city.x) * (this->x - city.x) +
+              (this->y - city.y) * (this->y - city.y));
+}
+
+const string Gene::toString() const {
+  return "{" + to_string(this->id) + ": (" + to_string(this->x) + ", " +
+         to_string(this->y) + ")}";
+}
+
+// Chromosome implementation
+
+Chromosome::Chromosome(const vector<Gene> &cities) {
+  this->DNA = cities;
+  evaluateFitness();
+}
+
+const void Chromosome::evaluateFitness() {
+  auto fitness = 0.0;
+  for (int i = 1; i < this->DNA.size(); ++i) {
+    fitness += this->DNA[i].calcDistance(this->DNA[i - 1]);
+  }
+  fitness += this->DNA[this->DNA.size() - 1].calcDistance(this->DNA[0]);
+  this->fitness = LOCATION_RANGE * LOCATION_RANGE / fitness;
+}
+
+const string Chromosome::toString() const {
+  string output = "[";
+  for (int i = 0; i < this->DNA.size() - 1; ++i) {
+    output += to_string(this->DNA[i].getId()) + ", ";
+  }
+  output += to_string(this->DNA[this->DNA.size() - 1].getId()) + "] -> " +
+            to_string(this->fitness);
+  return output;
+}
+
+const vector<Gene> &Chromosome::getDNA() const { return this->DNA; }
+
+const double Chromosome::getFitness() const { return this->fitness; }
+
+// functions implementation
+
+void initPopulation(const int chromosomesCount, const int genesCount) {
+  if (genesCount != 0) {
+    for (int i = 0; i < genesCount; ++i) {
+      int id = i;
+      double x = distributionLocation(gen);
+      double y = distributionLocation(gen);
+      originalDNA.emplace_back(id, x, y);
+    }
+    population.push_back(originalDNA);
+    for (int i = 1; i < chromosomesCount; ++i) {
+      auto tempDNA = originalDNA;
+      random_shuffle(tempDNA.begin(), tempDNA.end());
+      population.push_back(tempDNA);
+    }
+  } else {
+    for (int i = 0; i < chromosomesCount; ++i) {
+      auto tempDNA = originalDNA;
+      random_shuffle(tempDNA.begin(), tempDNA.end());
+      population.push_back(tempDNA);
+    }
+  }
+}
+
+void printDNA(const Chromosome &individual) {
+  out << individual.toString() << endl;
+  for (auto gene : individual.getDNA()) {
+    out << gene.toString() << endl;
+  }
+}
+
+void populationSort() {
+  stable_sort(population.begin(), population.end(),
+              [](const Chromosome &a, const Chromosome &b) -> bool {
+                return a.getFitness() > b.getFitness();
+              });
+}
+
+void selection() {
+  populationSort();
+  int selectionSize = (int)(POPULATION_SIZE * SELECTION_SIZE);
+  population.erase(population.begin() + selectionSize, population.end());
+  initPopulation(POPULATION_SIZE - selectionSize / 2 * 3);
+}
+
+void printIndividualWithBestFitness(const int generation) {
+  out << "\nGeneration " << generation << " with " << POPULATION_SIZE
+      << " individuals. Average fitness: " << avgFitness
+      << " -> Current best fitness:\n";
+  out << population[0].toString() << endl;
+}
+
+void crossover() {
+  random_shuffle(population.begin(), population.end());
+  int selectionSize = (int)(POPULATION_SIZE * SELECTION_SIZE);
+  for (int i = 0; i < selectionSize / 2; ++i) {
+    auto father = population[i].getDNA();
+    auto mother = population[population.size() - 1 - i].getDNA();
+    vector<Gene> child{father.begin(),
+                       father.begin() + (int)(father.size() * CROSSOVER_POINT)};
+    for (auto geneOfMother : mother) {
+      if (find_if(child.begin(), child.end(), [&](const Gene &gene) -> bool {
+            return gene.getId() == geneOfMother.getId();
+          }) != child.end()) {
+        continue;
+      }
+      child.push_back(geneOfMother);
+    }
+    population.push_back(child);
+  }
+}
+
+void mutation() {
+  uniform_int_distribution<int> distributionDNA{0, (int)population.size()};
+  double chance = distributionPercentage(gen);
+  if (chance <= MUTATION_CHANCE) {
+    swap(population[distributionDNA(gen)], population[distributionDNA(gen)]);
+    ++mutationCount;
+  }
+}
+
+void calcAvgFitness() {
+  avgFitness = 0.0;
+  for (auto individual : population) {
+    avgFitness += individual.getFitness();
+  }
+  avgFitness /= population.size();
+}
